564a586589e90c0779ee93d8333d7f716ddfe895
droufivem <82313613+droufivem@users.noreply.github.com>
2021-04-22 15:55:32 +0300

    Add files via upload

---
+++ good_hud/locales/en.lua
@@ -0,0 +1,72 @@
+-- English
+Locales['en'] = {
+	['you_are_on_location'] = 'Vriskese sto <strong>%s</strong>',
+
+	['date_format'] = {
+		default = 'Simera einai <strong>%s of %s, %s</strong>',
+		simple = 'Simera einai <strong>%s, %s</strong>',
+		simpleWithHours = 'Einai <strong>%s</strong>, <strong>%s, %s</strong>',
+		withWeekday = 'Simera einai <strong>%s, %s of %s, %s</strong>',
+		withHours = 'Simera einai <strong>%s</strong>, <strong>%s of %s, %s</strong>',
+		withWeekdayAndHours = 'Simera einai <strong>%s</strong>, <strong>%s, %s of %s, %s</strong>'
+	},
+
+	['weekDay_0'] = 'Kuriaki',
+	['weekDay_1'] = 'Deutera',
+	['weekDay_2'] = 'Trith',
+	['weekDay_3'] = 'Tetarti',
+	['weekDay_4'] = 'Pempti',
+	['weekDay_5'] = 'Paraskeuh',
+	['weekDay_6'] = 'Savato',
+
+	['day_1'] = '1',
+	['day_2'] = '2',
+	['day_3'] = '3',
+	['day_4'] = '4',
+	['day_5'] = '5',
+	['day_6'] = '6',
+	['day_7'] = '7',
+	['day_8'] = '8',
+	['day_9'] = '9',
+
+	['day_10'] = '10',
+	['day_11'] = '11',
+	['day_12'] = '12',
+	['day_13'] = '13',
+	['day_14'] = '14',
+	['day_15'] = '15',
+	['day_16'] = '16',
+	['day_17'] = '17',
+	['day_18'] = '18',
+	['day_19'] = '19',
+
+	['day_20'] = '20',
+	['day_21'] = '21',
+	['day_22'] = '22',
+	['day_23'] = '23',
+	['day_24'] = '24',
+	['day_25'] = '25',
+	['day_26'] = '26',
+	['day_27'] = '27',
+	['day_28'] = '28',
+	['day_29'] = '29',
+
+	['day_30'] = '30',
+	['day_31'] = '31',
+
+	['month_0'] = 'Ianouarios',
+	['month_1'] = 'Februarios',
+	['month_2'] = 'Martios',
+	['month_3'] = 'Aprilios',
+	['month_4'] = 'Maios',
+	['month_5'] = 'Iounios',
+	['month_6'] = 'Ioulios',
+	['month_7'] = 'Augustos',
+	['month_8'] = 'Septemvios',
+	['month_9'] = 'Oktovrios',
+	['month_10'] = 'Noemvrios',
+	['month_11'] = 'Dekemvrios',
+
+	['toggleui'] = 'Energopoihse/Apenergopihse komatia tou sustimatos HUD',
+
+}
