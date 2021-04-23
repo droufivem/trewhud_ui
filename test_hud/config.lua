27cb61022009e6ce25ea9b024efab6f6db033999
droufivem <82313613+droufivem@users.noreply.github.com>
2021-04-23 14:22:55 +0300

    ENJOY <3

---
+++ test_hud/config.lua
@@ -0,0 +1,70 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.serverLogo = 'https://i.imgur.com/XlkL3WR.png'
+
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= '~',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KMH',
+	maxSpeed = 320,
+
+	keys = {
+		seatbelt 	= 'B',
+		cruiser		= 'CAPS',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'DOWN',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= true,
+
+	showJob		 		= true,
+	showMafiaJob		 		= true,
+
+	showWalletMoney 	= true,
+	showBankMoney 		= true,
+	showBlackMoney 		= true,
+	showSocietyMoney	= true,
+	showMafiaSocietyMoney = true,
+
+	showDate 			= false,
+	showLocation 		= true,
+	showVoice	 		= false,
+
+	showHealth			= true,
+	showArmor	 		= true,
+	showStamina	 		= true,
+	showHunger 			= true,
+	showThirst	 		= true,
+
+	showMinimap			= false,
+
+	showWeapons			= true,	
+}
